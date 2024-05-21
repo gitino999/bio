f23ef94d95a7f38b9a5e75b23b2e92b052646f88
gitino999 <shingo-inoue@mori.co.jp>
2024-05-21 14:14:07 +0900

    html css js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function change1(){
+    document.getElementById("second").textContent = "琉球大学　生物多様性ビッグデータで日本全土の木本植物の個体数を推定"
+}
+function change2(){
+    document.getElementById("second").textContent = "Nice to meet you"
+}
